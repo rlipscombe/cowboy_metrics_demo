c3e8cafb5cd2b4729a533f643ad546325a8c3a59
Roger Lipscombe <roger@differentpla.net>
2023-01-15 21:40:15 +0000

    Log port number at startup

--- src/cowboy_metrics_demo_app.erl
+++ src/cowboy_metrics_demo_app.erl
@@ -4,17 +4,15 @@
 
 -export([start/2, stop/1]).
 
+-include_lib("kernel/include/logger.hrl").
+
+-define(PORT, 8190).
+
 start(_StartType, _StartArgs) ->
-    Dispatch = cowboy_router:compile([
-        {'_', [
-            {"/", home_handler, []}
-        ]}
-    ]),
-    {ok, _} = cowboy:start_clear(
-        http,
-        [{port, 8190}],
-        #{env => #{dispatch => Dispatch}}
-    ),
+    Dispatch = cowboy_router:compile([{'_', [{"/", home_handler, []}]}]),
+    {ok, _} = cowboy:start_clear(http, [{port, ?PORT}], #{env => #{dispatch => Dispatch}}),
+    ?LOG_INFO("Cowboy server listening on port ~p", [?PORT]),
+
     cowboy_metrics_demo_sup:start_link().
 
 stop(_State) ->
